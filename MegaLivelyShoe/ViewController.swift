2b173bcb2f53bd2adfff1bcfd3e2c9cea09f2bef
grdmn <groragrod.mn@hotmail.com>
2017-12-15 19:02:24 +0700

    Initial Commit

---
+++ MegaLivelyShoe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MegaLivelyShoe
+//
+//  Created by Grogrd Mn on 15/12/2560 BE.
+//  Copyright © 2560 Grogrd Mn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
